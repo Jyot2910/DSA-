e07bc608d3314285f560e2ff37f1dd0b8e5ab191
Jyot2910 <sheetalrathore1980@gmail.com>
2026-05-02 11:47:26 +0530

    Add files via upload

---
+++ palindrome_partioning.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool isPalin(string s) {
+    string s2 = s;             
+    reverse(s2.begin(), s2.end());  
+    return s == s2;
+}
+
+void getAllParts(string s, vector<string> &partitions, vector<vector<string>> &ans) {
+    if (s.size() == 0) {
+        ans.push_back(partitions);
+        return;
+    }
+
+    for (int i = 0; i < s.size(); i++) {
+        string part = s.substr(0, i + 1);
+
+        if (isPalin(part)) {
+            partitions.push_back(part);
+            getAllParts(s.substr(i + 1), partitions, ans);
+            partitions.pop_back();
+        }
+    }
+}
+
+int main() {
+    string s = "aab";
+
+    vector<vector<string>> ans;
+    vector<string> partitions;
+
+    getAllParts(s, partitions, ans);
+
+    for (auto vec : ans) {
+        for (auto str : vec) {
+            cout << str << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
